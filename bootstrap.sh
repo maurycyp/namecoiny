011433c043f1eca03660c0e8a0d1cc8cb41858a9
maurycyp <github.com@wayheavy.com>
2015-06-04 16:09:16 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,20 @@
+# update system
 apt-get update
 apt-get upgrade --yes
+
+# install tools
 apt-get install --yes wget
+pip install supervisor
+echo_supervisord_conf > /etc/supervisord.conf
+
+# install namecoin
 wget http://download.opensuse.org/repositories/home:p_conrad:coins/xUbuntu_14.04/Release.key
 apt-key add - < Release.key
 sh -c "echo 'deb http://download.opensuse.org/repositories/home:/p_conrad:/coins/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/namecoin.list"
 apt-get update
 apt-get install --yes namecoin
+
+# configure namecoin
 mkdir ~/.namecoin
 touch ~/.namecoin/namecoin.conf
 chmod 600 ~/.namecoin/namecoin.conf
